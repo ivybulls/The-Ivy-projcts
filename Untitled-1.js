ebab08897757b722f8028e3fa1721c434f06385e
Viva Yannick <ivivayan@gmail.com>
2026-04-14 21:48:38 +0200

    Revert "ivy addition "

--- Untitled-1.js
+++
@@ -1,9 +0,0 @@
-const str = "AABBACCDDBDE";
-const freqMap = new Map();
-
-for (let char of str) {
-  freqMap.set(char, (freqMap.get(char) || 0) + 1);
-}
-
-// Output: Map(5) { 'A' => 3, 'B' => 3, 'C' => 2, 'D' => 3, 'E' => 1 }
-console.log(freqMap);
